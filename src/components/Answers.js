331e7af905500ed8ead4407b77c0c03ed75b40b7
AlEkramHossainAbir <alabir65@gmail.com>
2023-06-04 17:51:16 +0600

    miniplayer added

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,38 +1,10 @@
-import { Fragment } from "react";
 import classes from "../styles/Answers.module.css";
 import Checkbox from "./Checkbox";
 
-export default function Answers({ options = [], handleChange, input }) {
+export default function Answers() {
   return (
     <div className={classes.answers}>
-      {options.map((option, index) => (
-        <Fragment key={index}>
-          {input ? (
-            <Checkbox
-              key={index}
-              className={classes.answer}
-              text={option.title}
-              value={index}
-              checked={option.checked}
-              onChange={(e) => handleChange(e, index)}
-            />
-          ) : (
-            <Checkbox
-              key={index}
-              className={`${classes.answer} ${
-                option.correct
-                  ? classes.correct
-                  : option.checked
-                  ? classes.wrong
-                  : null
-              } `}
-              text={option.title}
-              defaultChecked={option.checked}
-              disabled
-            />
-          )}
-        </Fragment>
-      ))}
+      <Checkbox className="answer" text="A New Hope 1" />
     </div>
   );
 }
